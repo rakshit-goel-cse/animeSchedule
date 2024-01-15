7a8f19767705cdb28053163706efbd4a04017918
Rakshit <55257622+rakshit-goel-cse@users.noreply.github.com>
2024-01-16 02:05:51 +0530

    added button in each card to save the route of anime in session storage

--- animeschedfront/src/components/ShowData.js
+++ animeschedfront/src/components/ShowData.js
@@ -5,25 +5,51 @@ function ShowData({data}) {
     console.log("Inside Show Data-",data);
     
     const englishName = (names) => {
-      return names.english;
+      if(names && 'english' in names){
+        return names.english;
+      }
+      return '';
+    }
+
+    const isRouteInAnimeList = (animeList, routeToCheck) => {
+      return animeList.some((anime) => anime === routeToCheck);
+    };
+
+    const addToList = (route) =>{
+      console.log("SAVING ANIME IN SESSION- ",route);
+      let storedlist=sessionStorage.getItem('animelist');
+      let animelist=[];
+      console.log("STORED LIST- ",storedlist);
+      if(storedlist){animelist=JSON.parse(storedlist);}
+        if(route){
+            if(isRouteInAnimeList(animelist,route)){return}
+            animelist=[...animelist,route];
+            sessionStorage.setItem('animelist',JSON.stringify(animelist));
+        }
     }
 
     return(
       <div>
       {data.map((item) => (
         <div key={item.id} className="card">
+          <div class="content">
+
 
           <h5>{item.title}<br></br>{englishName(item.names)}</h5>
           <h6>Dub Time - {item.dubTime}</h6>
+
+          
 
           <p>
           <b>episode-</b> {item.episodes} <br></br>
           <b>status-</b> {item.status} <br></br>
           <b>release year-</b> {item.year}
+          <button onClick={()=>{addToList(item.route)}}>Add to List</button>
           </p>
-          
+          </div>
           <img src={"https://img.animeschedule.net/production/assets/public/img/"+item.imageVersionRoute}
            alt={item.description}></img>
+         
           
         </div>
       ))}
